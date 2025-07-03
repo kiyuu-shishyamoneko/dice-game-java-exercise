7919908bc3ce0f8f98ab8bbb7bde6b545a538b58
KISHI Yushi <s1320031@linsv213.u-aizu.ac.jp>
2025-07-03 16:17:26 +0900

    Dice game: initial revisoin

--- DiceGame.java
+++ DiceGame.java
@@ -1,28 +1,21 @@
 import java.util.Random;
-import java.util.Scanner;
+import java.util.Scanner; 
+public class DiceGame {
 
-public class DiceGame2{
-    public static void main(String[] args){
-        int totalValue=rollDice();
-        if(totalValue>7){
-            System.out.println("You won!");
-        }else{
-            System.out.println("You lost.");
-        }
-        Scanner scanner=new Scanner(System.in);
-        scanner.close();
-    
+    public static void main(String[] args) {
+        int totalValue = rollDice();
     }
+    public static int rollDice() {
+        Random random = new Random();
 
-    public static int rollDice(){
-        Random random=new Random();
         System.out.println("Rolling dice...");
-        int d1=random.nextInt(6)+1;
-        int d2=random.nextInt(6)+1;
-        int total=d1+d2;
 
-        System.out.println("Die 1: " + d1);
-        System.out.println("Die 2: " + d2);
+        int die1 = random.nextInt(6) + 1; 
+        int die2 = random.nextInt(6) + 1;
+        int total = die1 + die2; 
+
+        System.out.println("Die 1: " + die1);
+        System.out.println("Die 2: " + die2);
         System.out.println("Total value: " + total);
 
         return total;
